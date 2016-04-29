bbbd2ed70f4b59629dac53474ec861f4d18f9849
phiree <phiree@gmail.com>
2016-04-29 08:50:01 +0800

    feat(ddd): 添加一个服务接口

--- IdentityAccess/ApplicationService/Membership/IMembershipService.cs
+++ IdentityAccess/ApplicationService/Membership/IMembershipService.cs
@@ -8,7 +8,13 @@ namespace IdentityAccess.ApplicationService.Membership
 {
   public   interface IMembershipService
     {
+        /// <summary>
+        /// 注册用户
+        /// </summary>
+        /// <param name="membership"></param>
+        /// <returns></returns>
         MembershipDto Register(MembershipDto membership);
         MembershipDto FindById(string id);
+        bool ValidateUser(MembershipDto membership);
     }
 }
